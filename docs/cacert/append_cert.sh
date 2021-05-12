d4cd5c2d411d64fff0d716be50d8293855d4663e
Francimar Alves <mazinsw@gmail.com>
2021-05-12 14:53:33 -0300

    feat: Adicionado novas cadeias de certificados

--- docs/cacert/append_cert.sh
+++ docs/cacert/append_cert.sh
@@ -5,9 +5,9 @@ FILE_LIST=$(find ./ -name "*.crt")
 cat cacert_mozilla.pem > cacert.pem
 for file in $FILE_LIST
 do
-    name=$(echo $file | cut -d / -f 2)
+    name=$(echo "$file" | cut -d / -f 2)
     echo "" >> cacert.pem
     echo "$name" >> cacert.pem
-    echo $name | sed -r 's/./=/g' >> cacert.pem
-    cat $file >> cacert.pem
+    echo "$name" | sed -r 's/./=/g' >> cacert.pem
+    cat "$file" >> cacert.pem
 done
