fb23594142632a4a108f5d2d4f158620d89ff391
Francimar Alves <mazinsw@gmail.com>
2024-05-14 16:22:59 -0300

    build(ibpt): atualizado tabela IBPT e cadeia de certificados

--- utils/update_IBPT.sh
+++ utils/update_IBPT.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-BASEDIR=$(dirname $0)
+BASEDIR=`pwd`/$(dirname $0)
 
-rm -f $BASEDIR/IBPT/*.*
-
-svn checkout https://github.com/frones/ACBr/trunk/Exemplos/ACBrTCP/ACBrIBPTax/tabela "$BASEDIR/IBPT"
+if [ -d "$BASEDIR/ACBr" ]; then
+    cwd=`pwd`
+    cd "$BASEDIR/ACBr" && git pull
+    cd $cwd
+else
+    git clone https://github.com/frones/ACBr "$BASEDIR/ACBr"
+    ln -sf "$BASEDIR/ACBr/Exemplos/ACBrTCP/ACBrIBPTax/tabela" "$BASEDIR/IBPT"
+fi
 
 php -f "$BASEDIR/update_IBPT.php" "$BASEDIR/IBPT" "$BASEDIR/../src/NFe/Database/data/IBPT"
